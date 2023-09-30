26e5156b2c85bd0b569ec789788dc7ad65221e8a
stephen <sdelius.prsvr@gmail.com>
2023-09-30 04:22:58 -0500

    the outputs are designed to be heard as if read by John Cleese

--- nodePractice.js
+++ nodePractice.js
@@ -8,6 +8,6 @@ console.log('Hello there, ' + firstName + ' ' + lastName + ".  You're precisely 
 
 console.log('Did you know "' + firstName.toUpperCase() + lastName.toUpperCase() + '"' + " is how your name would have been written in Ancient Rome?  No spacesandALLCAPS.  Makes you wonder if John Backus was really born in 1924, doesn't it?")
 
-console.log("By the way, do you have all your things monogrammed with " + firstName.charAt(0) + lastName.charAt(0) + "?  Oh dear.  Well, let's just hope no one else has those intitials.  Not that I can imagine why anyone would, what with how perfectly hideous they each look, let alone sitting there side by side.")
+console.log("By the way, do you have all your things monogrammed with " + firstName[0] + lastName[0] + "?  Oh dear.  Well, let's just hope no one else has those intitials.  Not that I can imagine why anyone would, what with how perfectly hideous they each look, let alone sitting there side by side.")
 
-console.log("Anyway, due to your tardiness, I'm afraid there wasn't any room left for you specifically on our private email server, so you'll just have to use " + '"' + (firstName.charAt(0)).toLowerCase() + lastName.toLowerCase() + '.prsvr@gmail.com" until someone is fired, quits, or dies.  Well, that should be everything.  Good luck, and whatever you do, don' + "'" + 't look under your desk.  Ta-ta!')
+console.log("Anyway, due to your tardiness, I'm afraid there isn't any room left for you specifically on our private email server, so you'll just have to use " + '"' + (firstName[0]).toLowerCase() + lastName.toLowerCase() + '.prsvr@gmail.com" until someone is fired, quits, or dies.  Well, that should be everything.  Good luck, and whatever you do, don' + "'" + 't look under your desk.  Ta-ta!')
