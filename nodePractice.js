e9553e879b18d893c05c76fc90de11f222b069e8
stephen <sdelius.prsvr@gmail.com>
2023-09-30 22:42:31 -0500

    the outputs are designed to be heard as if read by John Cleese

--- nodePractice.js
+++ nodePractice.js
@@ -6,7 +6,7 @@ const lastName = getInput(2)
 
 console.log('Hello there, ' + firstName + ' ' + lastName + ".  You're precisely on time.  On your time, that is.  By everyone else's time, you're quite late.")
 
-console.log('Did you know "' + firstName.toUpperCase() + lastName.toUpperCase() + '"' + " is how your name would have been written in Ancient Rome?  No spacesandALLCAPS.  Makes you wonder if John Backus was really born in 1924, doesn't it?")
+console.log('Did you know "' + firstName.toUpperCase() + lastName.toUpperCase() + '"' + " is how your name would have been written in Ancient Rome?  No spacesandALLCAPS.  Kind of makes you wonder if John Backus was born quite a bit before 1924, doesn't it?")
 
 console.log("By the way, do you have all your things monogrammed with " + firstName[0] + lastName[0] + "?  Oh dear.  Well, let's just hope no one else has those intitials.  Not that I can imagine why anyone would, what with how perfectly hideous they each look, let alone sitting there side by side.")
 
